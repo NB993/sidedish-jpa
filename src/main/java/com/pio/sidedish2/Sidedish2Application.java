113d39ea5dc8e198821b9c2458d1052a0abf5347
Jaehong Choe <qnalsfj@gmail.com>
2022-05-16 22:52:50 +0900

    init: first commit

---
+++ src/main/java/com/pio/sidedish2/Sidedish2Application.java
@@ -0,0 +1,13 @@
+package com.pio.sidedish2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sidedish2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sidedish2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pio/sidedish2/Sidedish2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pio.sidedish2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sidedish2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
